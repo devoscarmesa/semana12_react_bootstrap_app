3edb2ee9eb8e3a6f8f996bb060c9d52fc3f363be
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-07-27 02:34:50 -0500

    Updates

---
+++ precache-manifest.9b53216d9813ae0a070c7418d1cacc22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9520c58c4b5c1ab999638840fa19fb40",
+    "url": "/semana12_react_bootstrap_app/index.html"
+  },
+  {
+    "revision": "64a16fc88be113c4dde6",
+    "url": "/semana12_react_bootstrap_app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bd76fd182482938e0f3e",
+    "url": "/semana12_react_bootstrap_app/static/css/main.bc075b64.chunk.css"
+  },
+  {
+    "revision": "64a16fc88be113c4dde6",
+    "url": "/semana12_react_bootstrap_app/static/js/2.868c558d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/semana12_react_bootstrap_app/static/js/2.868c558d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd76fd182482938e0f3e",
+    "url": "/semana12_react_bootstrap_app/static/js/main.d760a1f1.chunk.js"
+  },
+  {
+    "revision": "92fcf6aa96bcc2e719ed",
+    "url": "/semana12_react_bootstrap_app/static/js/runtime-main.14d88c76.js"
+  }
+]);
